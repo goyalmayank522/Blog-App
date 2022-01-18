8aa8b0f48eedafb68d52efcbc7c3aef0e630ae3e
Mayank <goyalmayank522@gmail.com>
2022-01-18 12:01:11 +0530

    Comment feature added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+  
+  http_basic_authenticate_with name: "mayank", password: "bv-mayank", except: [:index, :show]
+
   def index
     @articles = Article.all
   end
